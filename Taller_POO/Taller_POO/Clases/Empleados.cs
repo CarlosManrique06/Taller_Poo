28103cf28fe2be70588853ed5e03b35e5415d8e2
CarlosManrique06 <cajumalo06@gmail.com>
2026-02-25 22:13:19 -0500

    Clase_Abstract

---
+++ Taller_POO/Taller_POO/Clases/Empleados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller_POO.Clases
+{
+    public abstract class Empleado
+    {
+        private int id;
+        private string nombre;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+    }
+}
